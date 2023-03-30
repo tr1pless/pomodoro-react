8e75b59e88c588365206e6f489d1424f7f27df06
kadmy95@gmail.com <kadmy95@gmail.com>
2023-03-30 15:00:49 +0300

    bugs fixed

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
 
-export const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
