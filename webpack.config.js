fb48af87fb8a9582061cc00934fad86cac7f15f5
kadmy95@gmail.com <kadmy95@gmail.com>
2023-08-07 15:13:04 +0300

    updated and build folder added

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.jsx"),
+  entry: path.resolve(__dirname, './src/index.jsx'),
   output: {
-    filename: "[name].bandle.[chunkhash].js",
+    filename: '[name].bandle.[chunkhash].js',
     clean: true,
-    path: path.resolve(__dirname, "./build"),
+    path: path.resolve(__dirname, './build'),
     environment: {
       arrowFunction: false,
     },
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
     alias: {
-      components: path.resolve(__dirname, "src/components/"),
-      src: path.resolve(__dirname, "src"),
-      store: path.resolve(__dirname, "src/store"),
+      components: path.resolve(__dirname, 'src/components/'),
+      src: path.resolve(__dirname, 'src'),
+      store: path.resolve(__dirname, 'src/store'),
     },
   },
   devServer: {
     compress: true,
-    port: 8000,
+    port: 7000,
     client: {
-      logging: "info",
+      logging: 'info',
     },
     historyApiFallback: true,
   },
-  devtool: "eval-source-map",
+  devtool: 'eval-source-map',
   module: {
     rules: [
       {
         test: /\.json$/,
-        use: ["json-loader"],
-        type: "javascript/auto",
+        use: ['json-loader'],
+        type: 'javascript/auto',
       },
       {
         test: /\.(png|jpg|gif)$/i,
-        type: "asset",
+        type: 'asset',
         parser: {
           dataUrlCondition: {
             maxSize: 8192,
@@ -47,21 +47,21 @@ module.exports = {
       {
         test: /\.(j|t)sx?$/,
         exclude: /node_module/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|gif)$/,
-        loader: "url-loader",
+        loader: 'url-loader',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
-};
+}
